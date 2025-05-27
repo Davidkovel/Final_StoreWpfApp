f0cf0d87b6f87b1a0dde6cd06e6a299fa40848f8
David <dkovel7@gmail.com>
2025-05-27 10:58:11 +0100

    Fixed some settings

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2466ae11e3792ae9a747ca925b2e2eeed9f7e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ee9ded9690924185a768252270b0ad032a7bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
