4c4ec32b6ba72304016086a5d8d4b7949d458043
David <dkovel7@gmail.com>
2025-06-17 18:27:21 +0100

    some config

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476e2f7e985307c4612194ef7430b1942ac1ec3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1136c3df739cc29ab77957723e11b7c0947661d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
